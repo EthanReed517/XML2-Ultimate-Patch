bd83c98f935dec107b299748293c7d2a1153aa52
EthanReed517 <ethanreed517@gmail.com>
2022-07-06 14:12:37 -0500

    Cyclops - Update CFG creation script
    Finalized and improved script

--- 1. Playable Characters/Cyclops/cfgCreate.py
+++ 1. Playable Characters/Cyclops/cfgCreate.py
@@ -1,35 +1,116 @@
-# Choose a mode
-# mode 1 = pre-defined
-# mode 2 = input
-mode = 2
+# ####### #
+# IMPORTS #
+# ####### #
+# To check if a file exists
+from os.path import exists
+# To be able to parse the ini file
+import configparser
 
-# Pre-Defined stuff
-# for future standardization, have these values pulled from a cfg file so that only one script needs to be used
-combatPackageName = "cyclops_0101"
-otherSkins = "0102,0103,0104,0105,0106,0107,0108,0109"
-createNCfromCombat = 0
-xboxIcons2 = 0
+# ######### #
+# FUNCTIONS #
+# ######### #
 
-# inputs
-if mode != 1:
-    combatPackageName = input('Input the name of the first combat package without the extension (ex: cyclops_0101)> ')
-    otherSkins = input('Input the other skin numbers, separated by commas (ex: 0102,0103,0104,0105,0106,0107,0108)> ')
-    createNCfromCombat = input('Do you want to create the _nc packages from the combat packages? [Y/N]> ')
-    if createNCfromCombat.lower() == 'y':
-        createNCfromCombat = 1
-    else:
-        createNCfromCombat = 0
-    xboxIcons2 = input('Do you want to use _icons2 for Xbox? [Y/N]> ')
-    if xboxIcons2.lower() == 'y':
-        xboxIcons2 = 1
-    else:
-        xboxIcons2 = 0
+# This function checks the settings in cfgSetup.ini
+def parseConfig():
+    config = configparser.ConfigParser()
+    config.read('cfgSetup.ini')
+    name = config['DEFAULT']['firstPackageName']
+    numbers = config['DEFAULT']['additionalSkins']
+    numbersList = numbers.split(",")
+    xboxIcons = config['DEFAULT']['xboxIcons2']
+    return [name,numbersList,xboxIcons]
 
-# generate names from packages
+# This function gets user input if an ini does not exist
+def userInput():
+    print("What is the name of the main combat package? Do not include file extensions (ex: cyclops_0101)")
+    name = input("Input: ")
+    print("What are the additional skin numbers? Enter a list separated by commas.")
+    print("(ex: 0102,0103,0104,0105,0106,0107,0108,0109)")
+    numbers = input("Input: ")
+    numbersList = numbers.split(",")
+    print("Do you want to use icons2 files for the Xbox non-combat packages?")
+    xboxIcons = input("[Y/N] ")
+    return [name,numbersList,xboxIcons]
 
+# This function gets the necessary information from the package
+def packageInfo(name):
+    nameFile = name + ".fb.cfg"
+    nameNCFile = name + "_nc.fb.cfg"
+    skinNo = name[name.index("_")+1:len(name)]
+    return [nameFile,nameNCFile,skinNo]
 
-# print variables for debug
-print('combatPackageName =',combatPackageName)
-print('otherSkins =',otherSkins)
-print('createNCfromCombat =',createNCfromCombat)
-print('xboxIcons2 =',xboxIcons2)
+# This function opens the package and returns its contents
+def openPackage(name):
+    with open(name, mode='r') as file:
+        fileContent = file.read()
+        contentList = fileContent.split("\n")
+        return contentList
+
+# This function takes in the existing package contents.
+# It uses this and the skin numbers to write a new package for the new skin.
+def packageRenumber(name,oldNo,newNo,contentList):
+    i = 0
+    newContentList = contentList
+    while i < len(newContentList):
+        if oldNo in newContentList[i]:
+            newContentList[i] = newContentList[i].replace(oldNo, newNo)
+        i +=1
+    nameOut = name.replace(oldNo,newNo)
+    with open(nameOut, mode='w') as file:
+        for line in newContentList:
+            file.write(line)
+            file.write('\n')
+
+# This function creates the Xbox _nc packages that use _icons2 instead of _icons1
+def xboxNC(name):
+    with open(name, mode='r') as file:
+        fileContent = file.read()
+        contentList = fileContent.split("\n")
+    i = 0
+    newContentList = contentList
+    while i < len(newContentList):
+        if "icons1" in newContentList[i]:
+            newContentList[i] = newContentList[i].replace("icons1", "icons2")
+        i +=1
+    nameOut = name + ".xbox"
+    with open(nameOut, mode='w') as file:
+        for line in newContentList:
+            file.write(line)
+            file.write('\n')
+
+# ############## #
+# MAIN EXECUTION #
+# ############## #
+
+# check if there is an ini file for this character
+iniExists = exists('cfgSetup.ini')
+if iniExists == True:
+    # get the contents of the ini file
+    [packageName,newNumbersList,xboxIcons2] = parseConfig()
+else:
+    # ask for the information
+    [packageName,newNumbersList,xboxIcons2] = userInput()
+
+# Generate the information about the package
+[fileName,ncFileName,oldNumber] = packageInfo(packageName)
+# Open the package and get its contents
+packageContents = openPackage(fileName)
+ncPackageContents = openPackage(ncFileName)
+
+# Generate the packages for the additional skins
+for newNumber in newNumbersList:
+    # For some reason, I have to call the openPackage function every time
+    # the loop is run.
+    # Otherwise, packageContents gets overwritten with the new package
+    # contents even though the variable is never changed
+    packageContents = openPackage(fileName)
+    packageRenumber(fileName,oldNumber,newNumber,packageContents)
+    ncPackageContents = openPackage(ncFileName)
+    packageRenumber(ncFileName,oldNumber,newNumber,ncPackageContents)
+
+# Xbox: use icons2 in the nc packages
+if xboxIcons2 == "yes":
+    fullSkinList = [oldNumber] + newNumbersList
+    for skin in fullSkinList:
+        openName = packageName.replace(oldNumber,skin) + '_nc.fb.cfg'
+        xboxNC(openName)
