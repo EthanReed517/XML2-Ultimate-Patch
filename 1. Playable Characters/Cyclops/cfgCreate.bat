299dc79eab5b05713cd88e841f9116717d2f5baa
EthanReed517 <ethanreed517@gmail.com>
2022-07-06 14:43:03 -0500

    Cyclops - Updates
    Update cfgCreate script, make it a global script, update info txt

--- 1. Playable Characters/Cyclops/cfgCreate.bat
+++ 1. Playable Characters/Cyclops/cfgCreate.bat
@@ -1,11 +1,13 @@
 @ECHO OFF
 
-move >nul "1. Base Assets\cyclops_0101.fb.cfg" .
-move >nul "1. Base Assets\cyclops_0101_nc.fb.cfg" .
+copy >nul "1. Base Assets\cyclops_0101.fb.cfg" .
+copy >nul "1. Base Assets\cyclops_0101_nc.fb.cfg" .
+
+copy >nul "..\..\0. Utilities\cfgCreate.py" .\
 
 python cfgCreate.py
 
 move >nul *.cfg "1. Base Assets"
-pause
 ren *xbox *.cfg
 move >nul *.cfg "2. Default Assets - Xbox"
+del cfgCreate.py
