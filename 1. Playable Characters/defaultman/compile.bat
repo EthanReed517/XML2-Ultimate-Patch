fdf1b7a28e834d881809b2b365ec0b1111c4fe5d
EthanReed517 <ethanreed517@gmail.com>
2022-08-03 21:00:59 -0500

    Defaultman - Update to new format

--- 1. Playable Characters/defaultman/compile.bat
+++ 1. Playable Characters/defaultman/compile.bat
@@ -12,11 +12,17 @@ REM console not selected, main compiler prompt not used, pick console
 :consoleChoicePrompt
 echo.
 echo Console not selected.
-echo [1] GameCube
-echo [2] PlayStation 2
-echo [3] PlayStation Portable (PSP)
-echo [4] Xbox
-set /p "consoleChoice=Which console are you using? [1,2,3,4] "
+echo [1] PC
+echo [2] GameCube
+echo [3] PlayStation 2
+echo [4] PlayStation Portable (PSP)
+echo [5] Xbox
+CHOICE /C 12345 /M "Which console are you using? "
+IF ERRORLEVEL 5 SET consoleChoice=XB & goto :section2
+IF ERRORLEVEL 4 SET consoleChoice=PSP & goto :section2
+IF ERRORLEVEL 3 SET consoleChoice=PS2 & goto :section2
+IF ERRORLEVEL 2 SET consoleChoice=GC & goto :section2
+IF ERRORLEVEL 1	SET consoleChoice=PC & goto :section2
 REM Checks if console was selected from main compiler script/if main compiler script was used
 :consoleChoiceCheck
 if "%consoleChoice%"=="" goto :consoleChoicePrompt
@@ -25,41 +31,35 @@ REM ***************************
 REM * Section 2 - Move Assets *
 REM ***************************
 
+:section2
 REM Begin compiling assets
 echo Compiling assets...
 md "0. Staging"
 robocopy >nul /e /v "1. Base Assets" "0. Staging"
 REM proceed based on console selection
-if %consoleChoice%==1 goto :compileGameCube
-if %consoleChoice%==2 goto :compilePS2
-if %consoleChoice%==3 goto :compilePSP
-if %consoleChoice%==4 goto :compileXbox
+if %consoleChoice%==PC goto :movePC
+if %consoleChoice%==GC goto :compileConsole
+if %consoleChoice%==PS2 goto :compileConsole
+if %consoleChoice%==PSP goto :movePSP
+if %consoleChoice%==XB goto :compileConsole
 
-REM GameCube options
-:compileGameCube
-robocopy >nul /e /v "2. Default Assets - GameCube" "0. Staging"
-goto :end
-
-REM PS2 options
-:compilePS2
-robocopy >nul /e /v "2. Default Assets - PS2" "0. Staging"
-goto :end
+REM PC options
+:movePC
+robocopy >nul /e /v "2. Default Assets - PC" "0. Staging"
+goto :compilePC
 
 REM PSP options
-:compilePSP
+:movePSP
 robocopy >nul /e /v "2. Default Assets - PSP" "0. Staging"
-goto :end
-
-REM xbox options
-:compileXbox
-robocopy >nul /e /v "2. Default Assets - Xbox" "0. Staging"
-goto :end
+goto :compileConsole
 
 REM ******************************
 REM * Section 3 - Compile Assets *
 REM ******************************
 
-:end
+:compileConsole
+REM can remove unneeded folders
+rmdir /s /q "0. Staging\1. Data Entries"
 copy >nul "..\..\0. Compilers" "0. Staging"
 REM change directory to 0. Staging folder, run fbbuilder, then change back to main directory
 cd "%~dp0\0. Staging"
@@ -74,3 +74,31 @@ cd ..
 REM move files and clean up
 robocopy >nul /e /v "0. Staging" "..\..\0. Ready Files\assetsfb Files"
 rmdir /s /q "0. Staging"
+
+goto :end
+
+:compilePC
+REM can remove unneeded folders
+rmdir /s /q "0. Staging\1. Data Entries"
+REM copy compilers
+copy >nul "..\..\0. Compilers" "0. Staging"
+REM change directory to 0. Staging folder and execute scripts
+cd "%~dp0\0. Staging"
+cmd /c ravenFormatsCompile.bat
+del /s >nul *.json
+REM clean up extra stuff
+del >nul *.cfg
+del >nul enter.vbs
+del >nul fbbuilder.bat
+del >nul ravenFormatsCompile.bat
+REM move back to the main folder
+cd ..
+
+REM move files and clean up
+robocopy >nul /e /v "0. Staging" "..\..\0. Ready Files"
+rmdir /s /q "0. Staging"
+
+goto :end
+
+:end
+echo Transfer Complete
---
+++ 1. Playable Characters/defaultman/update.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+
+echo Copying data entries to data files...
+copy >nul "1. Base Assets\1. Data Entries\Herostat_Defaultman.txt" "..\..\3. General Data Files\herostat\1. Base Assets\0. Herostat Entries\Defaultman.txt"
