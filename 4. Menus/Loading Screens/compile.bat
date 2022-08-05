bf7b6f82423332359d65bd4589daa6d54387673a
EthanReed517 <ethanreed517@gmail.com>
2022-08-04 21:15:48 -0500

    Comics and Loading Screens - New Compiler

--- 4. Menus/Loading Screens/compile.bat
+++ 4. Menus/Loading Screens/compile.bat
@@ -12,11 +12,17 @@ REM console not selected, main compiler prompt not used, pick console
 :consoleChoicePrompt
 echo.
 echo Console not selected.
-echo [1] GameCube
-echo [2] PlayStation 2
-echo [3] PlayStation Portable (PSP)
-echo [4] Xbox
-set /p "consoleChoice=Which console are you using? [1,2,3,4] "
+echo [1] PC
+echo [2] GameCube
+echo [3] PlayStation 2
+echo [4] PlayStation Portable (PSP)
+echo [5] Xbox
+CHOICE /C 12345 /M "Which console are you using? "
+IF ERRORLEVEL 5 SET consoleChoice=XB & goto :section2
+IF ERRORLEVEL 4 SET consoleChoice=PSP & goto :section2
+IF ERRORLEVEL 3 SET consoleChoice=PS2 & goto :section2
+IF ERRORLEVEL 2 SET consoleChoice=GC & goto :section2
+IF ERRORLEVEL 1	SET consoleChoice=PC & goto :section2
 REM Checks if console was selected from main compiler script/if main compiler script was used
 :consoleChoiceCheck
 if "%consoleChoice%"=="" goto :consoleChoicePrompt
@@ -24,25 +30,34 @@ if "%consoleChoice%"=="" goto :consoleChoicePrompt
 REM ***************************
 REM * Section 2 - Move Assets *
 REM ***************************
-if %consoleChoice%==1 goto :compileGameCube
-if %consoleChoice%==2 goto :compilePS2
-if %consoleChoice%==3 goto :compilePSP
-if %consoleChoice%==4 goto :compileXbox
+:section2
+if %consoleChoice%==PC goto :movePC
+if %consoleChoice%==GC goto :moveGameCube
+if %consoleChoice%==PS2 goto :movePS2
+if %consoleChoice%==PSP goto :movePSP
+if %consoleChoice%==XB goto :moveXbox
 
-:compileGameCube
-robocopy >nul /e /v "2. Default Assets - GameCube" "..\..\0. Ready Files\assetsfb Files"
+:movePC
+robocopy >nul /e /v "1. Base Assets" "..\..\0. Ready Files"
 goto :end
 
-:compilePS2
-robocopy >nul /e /v "2. Default Assets - PS2" "..\..\0. Ready Files\assetsfb Files"
+:moveGameCube
+robocopy >nul /e /v "2. Default Assets - GameCube\textures" "..\..\0. Ready Files\assetsfb Files\textures"
+robocopy >nul /e /v "2. Default Assets - GameCube\2. Bonus Loading Screens" "..\..\0. Ready Files\2. Bonus Loading Screens"
 goto :end
 
-:compilePSP
-robocopy >nul /e /v "2. Default Assets - PSP" "..\..\0. Ready Files\assetsfb Files"
+:movePS2
+robocopy >nul /e /v "2. Default Assets - PS2\textures" "..\..\0. Ready Files\assetsfb Files\textures"
+robocopy >nul /e /v "2. Default Assets - PS2\2. Bonus Loading Screens" "..\..\0. Ready Files\2. Bonus Loading Screens"
 goto :end
 
-:compileXbox
-robocopy >nul /e /v "2. Default Assets - Xbox" "..\..\0. Ready Files\assetsfb Files"
+:movePSP
+robocopy >nul /e /v "2. Default Assets - PSP\textures" "..\..\0. Ready Files\assetsfb Files\textures"
+goto :end
+
+:moveXbox
+robocopy >nul /e /v "1. Base Assets\textures" "..\..\0. Ready Files\assetsfb Files\textures"
+robocopy >nul /e /v "1. Base Assets\2. Bonus Loading Screens" "..\..\0. Ready Files\2. Bonus Loading Screens"
 goto :end
 
 :end
