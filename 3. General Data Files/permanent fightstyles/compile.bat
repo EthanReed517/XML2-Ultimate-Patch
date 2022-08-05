83260a071db551c37168518e12654e73c189f91a
EthanReed517 <ethanreed517@gmail.com>
2022-08-04 20:48:14 -0500

    Permanent_fightstyles - new compiler

--- 3. General Data Files/permanent fightstyles/compile.bat
+++ 3. General Data Files/permanent fightstyles/compile.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Compiling permanent.fb
+echo Compiling permanent_fightstyles.fb
 
 REM ***********************************
 REM * Section 1 - Establish Variables *
@@ -12,11 +12,17 @@ REM console not selected, main compiler prompt not used, pick console
 :consoleChoicePrompt
 echo.
 echo Console not selected.
-echo [1] GameCube
-echo [2] PlayStation 2
-echo [3] PlayStation Portable (PSP)
-echo [4] Xbox
-set /p "consoleChoice=Which console are you using? [1,2,3,4] "
+echo [1] PC
+echo [2] GameCube
+echo [3] PlayStation 2
+echo [4] PlayStation Portable (PSP)
+echo [5] Xbox
+CHOICE /C 12345 /M "Which console are you using? "
+IF ERRORLEVEL 5 SET consoleChoice=XB & goto :section2
+IF ERRORLEVEL 4 SET consoleChoice=PSP & goto :section2
+IF ERRORLEVEL 3 SET consoleChoice=PS2 & goto :section2
+IF ERRORLEVEL 2 SET consoleChoice=GC & goto :section2
+IF ERRORLEVEL 1	SET consoleChoice=PC & goto :section2
 REM Checks if console was selected from main compiler script/if main compiler script was used
 :consoleChoiceCheck
 if "%consoleChoice%"=="" goto :consoleChoicePrompt
@@ -24,25 +30,40 @@ if "%consoleChoice%"=="" goto :consoleChoicePrompt
 REM ***************************
 REM * Section 2 - Move Assets *
 REM ***************************
-
+:section2
 REM Begin compiling assets
 echo Compiling assets...
 md "0. Staging"
 robocopy >nul /e /v "1. Base Assets" "0. Staging"
-goto :end
+if %consoleChoice%==PC goto :movePC
+if %consoleChoice%==PSP goto :movePSP
+goto :compileConsole
+
+REM PC options
+:movePC
+robocopy >nul /e /v "2. Default Assets - PC" "0. Staging"
+goto :compilePC
+
+REM PSP options
+:movePSP
+robocopy >nul /e /v "2. Default Assets - PSP" "0. Staging"
+goto :compileConsole
 
 REM ******************************
 REM * Section 3 - Compile Assets *
 REM ******************************
 
-:end
+:compileConsole
 copy >nul "..\..\0. Compilers" "0. Staging"
 REM change directory to 0. Staging folder, run fbbuilder, then change back to main directory
 cd "%~dp0\0. Staging"
+cmd /c ravenFormatsCompile.bat
+del /s >nul *.json
 call fbbuilder.bat
 del >nul *.cfg
 del >nul enter.vbs
 del >nul fbbuilder.bat
+del >nul ravenFormatsCompile.bat
 md "packages\generated\maps\package"
 for /r %%x in (*.fb) do move >nul "%%x" "packages\generated\maps\package"
 cd ..
@@ -54,3 +75,30 @@ rmdir /s /q "0. Staging/data"
 REM move files and clean up
 robocopy >nul /e /v "0. Staging" "..\..\0. Ready Files\assetsfb Files"
 rmdir /s /q "0. Staging"
+
+goto :end
+
+
+:compilePC
+REM copy compilers
+copy >nul "..\..\0. Compilers" "0. Staging"
+REM change directory to 0. Staging folder and execute scripts
+cd "%~dp0\0. Staging"
+REM compile data files
+cmd /c ravenFormatsCompile.bat
+del /s >nul *.json
+REM clean up extra stuff
+del >nul *.cfg
+del >nul enter.vbs
+del >nul fbbuilder.bat
+del >nul ravenFormatsCompile.bat
+REM move back to the main folder
+cd ..
+
+REM move files and clean up
+robocopy >nul /e /v "0. Staging" "..\..\0. Ready Files"
+rmdir /s /q "0. Staging"
+
+goto :end
+
+:end
