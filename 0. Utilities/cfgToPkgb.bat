743186e1447e663f1ac3f99d5d1ed3c546cec991
EthanReed517 <ethanreed517@gmail.com>
2022-09-18 16:20:11 -0500

    CFG to PKGB script - add auto move

--- 0. Utilities/cfgToPkgb.bat
+++ 0. Utilities/cfgToPkgb.bat
@@ -3,3 +3,6 @@
 for %%i in (*.cfg) do (
 	echo %%~ni | python cfgToPkgb.py
 )
+
+md "..\2. Default Assets - PC\packages\generated\characters"
+for %%x in (*.json) do move >nul "%%x" "..\2. Default Assets - PC\packages\generated\characters"
