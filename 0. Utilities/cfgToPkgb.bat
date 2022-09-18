3bfecb8e34f2a125022015bb4e03aa577f46ef49
EthanReed517 <ethanreed517@gmail.com>
2022-09-18 16:32:39 -0500

    CFG to PKGB script - better copying
    Now can copy the py script on its own

--- 0. Utilities/cfgToPkgb.bat
+++ 0. Utilities/cfgToPkgb.bat
@@ -1,8 +1,29 @@
 @ECHO OFF
 
+for %%I in (.) do set CurrDirNameFull=%%~fI
+
+:dircheck
+for %%I in (.) do set CurrDirName=%%~nxI
+if not "%CurrDirName%"=="XML2-Ultimate-Patch" (
+	cd ..
+	goto dircheck
+)
+
+copy >nul "0. Utilities\cfgToPkgb.py" "%CurrDirNameFull%"
+
+echo DEBUG
+pause
+
+cd %CurrDirNameFull%
+
 for %%i in (*.cfg) do (
 	echo %%~ni | python cfgToPkgb.py
 )
+
+echo DEBUG 2
+pause
+
+del >nul cfgToPkgb.py
 
 md "..\2. Default Assets - PC\packages\generated\characters"
 for %%x in (*.json) do move >nul "%%x" "..\2. Default Assets - PC\packages\generated\characters"
