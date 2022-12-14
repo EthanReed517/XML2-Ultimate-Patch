e80a7bfc6a69db021c155334b576e163251e3f31
EthanReed517 <ethanreed517@gmail.com>
2022-12-14 17:42:18 -0600

    Cable - New compiler

---
+++ 1. Playable Characters/Cable/0. Simple Compilers/compile_Cable.bat
@@ -0,0 +1,35 @@
+@echo off
+echo Compiling Playable Character - Cable
+
+REM **************************
+REM * Section 0 - User Input *
+REM **************************
+
+REM get the name of the character and their number
+set charName=cable
+set charNum=115
+REM define other values for the compiler
+set effectsFolder=cable
+set loadingScreen1=11501
+set loadingScreen2=""
+set loadingScreen3=""
+set powerstyle=ps_cable
+set soundFile=cable_m
+
+REM fill in if the character needs a special compilation process on consoles
+REM (like Cyclops with his skin-specific visor)
+REM options are Y or N
+set specialCompile=N
+REM if the character has a special compiler process, list the name here (no file extension)
+set specialCompileName=""
+
+REM ******************************
+REM * Section 1 - Main Execution *
+REM ******************************
+REM copy the compiler
+copy >nul "0. Compilers\1. Playable Characters\compileCommon.bat" "%~dp0"
+REM execute the compiler
+call compileCommon.bat
+REM delete the compiler
+del >nul compileCommon.bat
+echo.
