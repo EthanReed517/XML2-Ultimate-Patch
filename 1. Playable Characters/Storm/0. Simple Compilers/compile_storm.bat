24550c07cf8a74dcad82d0e8bc9fd5d209515e20
Enigma1st <tnjm5772@gmail.com>
2022-12-27 23:43:26 -0600

    Storm - compilers

---
+++ 1. Playable Characters/Storm/0. Simple Compilers/compile_storm.bat
@@ -0,0 +1,35 @@
+@echo off
+echo Compiling Playable Character - Storm
+
+REM **************************
+REM * Section 0 - User Input *
+REM **************************
+
+REM get the name of the character and their number
+set charName=storm
+set charNum=04
+REM define other values for the compiler
+set effectsFolder=storm
+set loadingScreen1=0401
+set loadingScreen2=0402
+set loadingScreen3=0403
+set powerstyle=ps_storm
+set soundFile=storm_m
+
+REM fill in if the character needs a special compilation process on consoles
+REM (like Cyclops with his skin-specific visor)
+REM options are Y or N
+set specialCompile=N
+REM if the character has a special compiler process, list the name here (no file extension)
+set specialCompileName=""
+
+REM ******************************
+REM * Section 1 - Main Execution *
+REM ******************************
+REM copy the compiler
+copy >nul "0. Compilers\1. Playable Characters\compileCommon.bat" "%~dp0"
+REM execute the compiler
+call compileCommon.bat
+REM delete the compiler
+del >nul compileCommon.bat
+echo.
