a1c9f9ad3c712503355d2a7a6dd2f5aa464dcb51
Enigma1st <tnjm5772@gmail.com>
2023-05-28 19:30:42 -0500

    Shifter - compilers

---
+++ 2. NPCs/Shifter/0. Simple Compilers/compile_shifter_b.bat
@@ -0,0 +1,32 @@
+@echo off
+echo Compiling NPC - Shifter_b
+
+REM **************************
+REM * Section 0 - User Input *
+REM **************************
+
+REM get the name of the character and their number
+set charName=shifter_b
+set charNum=67
+REM define other values for the compiler
+set effectsFolder=shfter
+set powerstyle=ps_shifter
+set soundFile=shfter_m
+
+REM fill in if the character needs a special compilation process on consoles
+REM (like Cyclops with his skin-specific visor)
+REM options are Y or N
+set specialCompile=N
+REM if the character has a special compiler process, list the name here (no file extension)
+set specialCompileName=""
+
+REM ******************************
+REM * Section 1 - Main Execution *
+REM ******************************
+REM copy the compiler
+copy >nul "0. Compilers\2. NPCs\compileCommon.bat" "%~dp0"
+REM execute the compiler
+call compileCommon.bat
+REM delete the compiler
+del >nul compileCommon.bat
+echo.
