2b22da3ceb5d83ac205d5e53340623bbb30c4975
Ethan <ethanreed517@gmail.com>
2024-10-14 11:14:47 -0500

    Remove old files
    Remove some old/unneeded compiler files used for data files.

--- 3. General Data Files/shared_talents/update.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-copy >nul "1. Base Assets\data\shared_talents.engb.json" "..\permanent\1. Base assets\data"
--- 3. General Data Files/zoneinfo/update.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-
-copy >nul "1. Base Assets\data\zoneinfo.engb.json" "..\permanent\1. Base assets\data"
